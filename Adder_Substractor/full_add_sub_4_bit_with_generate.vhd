8d29f7d2c0350a44f612364df2aaecb234d70512
Chedly Zouche <chedlyzouche9999@outlook.com>
2021-05-08 21:07:20 +0100

    Added generate method for Adder-Substractor

--- Adder_Substractor/full_add_sub_4_bit_with_generate.vhd
+++ Adder_Substractor/full_add_sub_4_bit_with_generate.vhd
@@ -37,25 +37,23 @@ Port (
   s : out STD_LOGIC );
 end component;
 -- signals
-singal b0x, b1x, b2x, b3x : STD_LOGIC;
-singal c0, c1, c2, c3, c4 : STD_LOGIC;
+singal b0x, b1x, b2x, b3x : STD_LOGIC_VECTOR(3 downto 0);
+singal c0, c1, c2, c3, c4 : STD_LOGIC(4 downto 0);
 singal ovf_m : STD_LOGIC;
 BEGIN
-c0 <= M;
+c(0) <= M;
 --Add/Sub xor
-xor_0: xor_custom port map (B(0) => a, M => b, b0x ->s);
-xor_1: xor_custom port map (B(1) => a, M => b, b1x ->s);
-xor_2: xor_custom port map (B(2) => a, M => b, b2x ->s);
-xor_3: xor_custom port map (B(3) => a, M => b, b3x ->s);
+gen1: for i in 0 to 3 generate 
+xor_custom port map(B(i) => a, M => b, b(i) => s)
+end generate gen1;
 --ovf xor
 xor_4: xor_custom port map (c4 => a, c3 => b, ovf_m ->s);
 --mux
 mux: mux port map(M => sel, in_0 => c4, in_1 => ovf_m, MCOV => s);
 --Adders
-add_0: full_add port map(A(0) => a, b0x => b, c0 => cin, S(0)=>s, c1 => cout);
-add_1: full_add port map(A(1) => a, b1x => b, c1 => cin, S(1)=>s, c2 => cout);
-add_2: full_add port map(A(2) => a, b2x => b, c2 => cin, S(2)=>s, c3 => cout);
-add_3: full_add port map(A(3) => a, b3x => b, c3 => cin, S(3)=>s, c4 => cout);
-CF <= c4;
+gen2 : for i in 0 to 3 generate
+full_add port map(A(i) => a, b(i)=> b, c(i) => cin, S(i) => s, c(i+1) => cout );
+end generate gen2;
+CF <= c(4);
 OVF <= ovf_m;
 END behavior ;
